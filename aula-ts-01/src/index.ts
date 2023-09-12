47c5a2eaa38f57b3842c4fcda83d3faa663542ce
Willyan <wkinhaamaral@gmail.com>
2023-09-12 13:33:12 -0300

    aula-ts-03

--- aula-ts-01/src/index.ts
+++ aula-ts-01/src/index.ts
@@ -1,6 +1,6 @@
-function somar(num1: number, num2: number) {
+function somar1(num1: number, num2: number) {
     return num1 + num2;
 }
 
-const resultado = somar(2, 3);
-console.log(resultado);
+const resultado1 = somar1(2, 3);
+console.log(resultado1);
